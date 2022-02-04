30ffe079bbe08636440c28636f51a9a7801673f7
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2022-02-04 15:38:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/fy0F9szK/114-1147898-clown-nose-png-clip-art-clown-nose-transparent-removebg-preview.png');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initilalized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x-20;
+        noseY = results[0].pose.nose.y-10;
+        console.log("nose-x = " + noseX);
+        console.log("nose-y = " + noseY);
+
+    }
+
 }
 
 function draw() {
-
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 50, 30);
 }
 
 function take_snapshot() {
